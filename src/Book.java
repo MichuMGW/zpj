1f863f97c7b5a414f9a0379c49c1be8aab89d193
MichuMGW <iver3n@gmail.com>
2025-10-19 19:36:07 +0200

    Laboratorium 2

--- src/Book.java
+++ src/Book.java
@@ -4,6 +4,8 @@ public class Book {
     private int year;
     private double price;
 
+    public Book(){}
+
     public Book(String title, String author, int year, double price) {
         this.title = title;
         this.author = author;
@@ -15,6 +17,22 @@ public class Book {
     public String getAuthor() { return author; }
     public int getYear() { return year; }
     public double getPrice() { return price; }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
     @Override
     public String toString() {
